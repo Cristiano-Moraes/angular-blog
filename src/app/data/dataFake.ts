6cfdb063c5dd9d5943384e8938127b1e93925072
Cristiano-Moraes <crs.moraes@gmail.com>
2023-06-04 18:13:49 -0300

    commit da versao inicial do otakus blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,9 +1,9 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "ONE PUNCH MAN 3 TEMPORADA CONFIRMADA",
+    "description":"Mas afinal, Qual Studio será o encarregado de Animar a 3 Temporada de One Punch man?",
+    "photoCover":"https://gamepress.com.br/wp-content/uploads/2019/06/One-Punch-Man-cover.png"
   },
   {
     "id":"2",
